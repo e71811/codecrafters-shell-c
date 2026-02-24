7cb582332a40b499d73dfed6b6e9b055fab48942
ilay <ilay.helfgott@live.ac.il>
2026-02-24 15:04:36 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -14,15 +14,20 @@ int main(int argc, char *argv[]) {
        fgets(buffer, sizeof(buffer), stdin);
       //sets the input so i can interact with it later
          char userInput[sizeof(buffer)];
+      // sets buit in commands
+      char *builtins[] = {"exit", "echo", "type", NULL};
          for(int i = 0; i < strlen(buffer); i++)
          {
            if(buffer[i] == '\n')
            {
              userInput[i] = '\0';
+             // exit command
              if (strcmp(userInput,"exit")==0)
              {
                return 0 ;
              }
+
+             // echo command
              if (userInput[0] == 'e' && userInput[1] == 'c' && userInput[2] == 'h' && userInput[3] == 'o')
              {
                for (int i = 5; i < strlen(userInput); i++)
@@ -31,6 +36,30 @@ int main(int argc, char *argv[]) {
                }
                printf("\n");
                break;
+             }
+
+             // Type command
+             if (strncmp(userInput,"type ",5) == 0)
+             {
+               const char s[2] = " ";
+               char *token;
+               token = strtok(userInput, s);
+               token = strtok(NULL, s);
+               int flag = 0;
+               for (int i = 5; i < sizeof(builtins); i++)
+               {
+                 if(strcmp(token,builtins[i]) == 0)
+                 {
+                   printf("%s ", builtins[i]);
+                   printf("is a shell builtin");
+                   flag = 1;
+                 }
+               }
+               if ( flag==0)
+               {
+                 printf("%s: not found", token);
+               }
+
              }
              printf("%s",userInput);
              printf(": command not found \n");
